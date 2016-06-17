e8e79db23d564ca76a45fb0188eb39e5e582f985
Sage Berg <sberg@uoregon.edu>
2016-06-16 17:25:29 -0700

    Change file extension for transipled JavaScript files

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ./node_modules/.bin/babel client.js > babelified_client.js
-browserify babelified_client.js > client.min.js
-browserify unit_tests.js > unit_tests.min.js
-browserify fixed_header.js > fixed_header.min.js
+browserify babelified_client.js > client.js.browserified
+browserify unit_tests.js > unit_tests.js.browserified
+browserify fixed_header.js > fixed_header.js.browserified
